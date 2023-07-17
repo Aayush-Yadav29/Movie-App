302e5827ea4179b4e809732e505da55f0f098d39
Aayush-Yadav29 <aayush.yadav.21031@iitgoa.ac.in>
2023-07-17 18:48:51 +0530

    updating package.json

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
 import Leftbar from './Leftbar';
 import MainPanel from './MainPanel';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Search from './Search';
+
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Leftbar/>
-      <Switch>
-        <Route exact path = "/">
-          <MainPanel/>
-        </Route>
-        <Route exact path = "/Search">
-          <Search/>
-        </Route>
-      </Switch>
-    </div>
+      <div className="App">
+        <Leftbar />
+        <Switch>
+          <Route exact path="/Movie-app">
+            <MainPanel />
+          </Route>
+          <Route exact path="/Search">
+            <Search />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
