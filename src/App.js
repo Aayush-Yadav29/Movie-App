34426c6c06265663f62c90bcdc5f51da98cfbbb4
Aayush-Yadav29 <aayush.yadav.21031@iitgoa.ac.in>
2023-08-07 23:31:58 +0530

    updating routes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import Leftbar from './Leftbar';
 import MainPanel from './MainPanel';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Search from './Search';
-
+import Tv from './Tv';
+import Sports from './Sports';
+import TrailerPage from './TrailerPage';
 function App() {
   return (
     <Router>
       <div className="App">
         <Leftbar />
         <Switch>
-          <Route exact path="/Movie-app">
+          <Route exact path="/">
+            <MainPanel />
+          </Route>
+          <Route exact path="/MainPanel">
             <MainPanel />
           </Route>
           <Route exact path="/Search">
             <Search />
+          </Route>
+          <Route exact path="/Tv">
+            <Tv/>
+          </Route>
+          <Route exact path="/Sports">
+            <Sports/>
+          </Route>
+          <Route exact path="/MainPanel/:id">
+            <TrailerPage/>
           </Route>
         </Switch>
       </div>
