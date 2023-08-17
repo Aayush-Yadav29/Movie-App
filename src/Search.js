969912369bc4c729503e73883e14cf50d670ffad
Aayush-Yadav29 <aayush.yadav.21031@iitgoa.ac.in>
2023-08-17 13:44:53 +0530

    updating search page

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Search.css";
 import config from './config.json';
-// import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
+
 
 const Search = () => {
   const MOVIE_API = "https://api.themoviedb.org/3/";
@@ -93,24 +90,6 @@ const Search = () => {
         </IconButton>
       </Paper>
       </div>
-      {/* <Paper
-        component="form"
-        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
-      >
-        <IconButton sx={{ p: '10px' }} aria-label="menu">
-        </IconButton>
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Search"
-          inputProps={{ 'aria-label': 'search google maps' }}
-        />
-        <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={handleSearchIconClick}>
-          <SearchIcon />
-        </IconButton>
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions"> 
-        </IconButton>
-      </Paper> */}
 
       <main>
         <div className={"center-max-size container"}>
@@ -128,7 +107,7 @@ const Search = () => {
                     )}
                   </div>
                 ))
-                : <p>Sorry, no movies found</p>
+                : <p className="text">Sorry, no movies found</p>
               : discoverResults.length > 0
                 ? discoverResults.map((movie) => (
                   <div key={movie.id} className="movie">
@@ -141,7 +120,7 @@ const Search = () => {
                     )}
                   </div>
                 ))
-                : <p>Loading...</p>}
+                : <p className="text">Loading...</p>}
           </div>
         </div>
       </main>
